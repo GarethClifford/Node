70952bcc861cf23574238207bd41954b6b855512
Gareth <gareth.clifford@live.co.uk>
2018-11-07 17:10:23 +0000

    Fully working

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,25 +4,28 @@ const open = require("open");
 
 const axios = require('axios');
 
-axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=355383a6').then(function(response){
-    console.log(response.data);
-    console.log(response.status);
-  });
+// axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=355383a6').then(function(response){
+//     console.log(response.data);
+//     console.log(response.status);
+//   });
 
 const port = 3000;
 const app = express();
+const apiTitle = 'http://www.omdbapi.com/?t=';
+const apiSearch = 'http://www.omdbapi.com/?s=';
 
-//<input id+->
 
 
 app.get("/get/:title", (req, res) => {
- // res.sendFile(path.join(__dirname,"../src/index.html"));
- axios.get('http://www.omdbapi.com/?t='+ req.param("title") + '&apikey=355383a6').then(response => res.send(response.data))
+ axios.get('http://www.omdbapi.com/?t=' + req.param("title") + '&apikey=355383a6').then(response => res.send(response.data))
+});
+app.get("/search/:search", (req, res) => {
+ axios.get('http://www.omdbapi.com/?s=' + req.param("search") + '&apikey=355383a6').then(response => res.send(response.data))
 });
 app.get("/", (req, res) => {
  res.sendFile(path.join(__dirname,"../src/index.html"));
- // axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=355383a6').then(response => res.send(response.data))
 });
+
 app.listen(port, (err) => {
  if(err){
  console.log(err);
